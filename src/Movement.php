9f064affb1747b4df93fe922e100eed68633e80f
Matt Lantz <mattlantz@gmail.com>
2016-07-11 23:44:30 -0400

    basic movement trait

---
+++ src/Movement.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Yab\Cerebrum;
+
+use Carbon\Carbon;
+use Illuminate\Support\Facades\Cache;
+
+trait Movement
+{
+    protected $connection = '';
+
+    public function clone()
+    {
+
+    }
+
+    public function backup()
+    {
+
+    }
+
+    public function transfer()
+    {
+
+    }
+}
