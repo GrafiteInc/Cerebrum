fb991ce93ebdd42e4a24449cccbd60e18a07d887
Matt Lantz <mattlantz@gmail.com>
2016-07-11 23:48:20 -0400

    Basic plan

---
+++ src/Linguistics.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace Yab\Cerebrum;
+
+use Carbon\Carbon;
+
+trait Linguistics
+{
+    public function getKeywords()
+    {
+
+    }
+
+    public function getSentences()
+    {
+
+    }
+
+    public function hasConfirmed()
+    {
+
+    }
+
+    public function getWords()
+    {
+
+    }
+
+    public function isQuestion()
+    {
+
+    }
+
+    public function isCompleteSentence()
+    {
+
+    }
+
+    public function getAttitude()
+    {
+
+    }
+
+    public function getStem()
+    {
+
+    }
+
+    public function isPositive()
+    {
+
+    }
+
+    public function isNegative()
+    {
+
+    }
+
+    public function containsProfanity()
+    {
+
+    }
+}
